a445e277fb0d40a8ab2091e7dd3a771ed58e6ff6
mamartam <marta.masiukk@gmail.com>
2024-09-26 16:38:12 +0300

    added project1 and project2 folder; did first of them

---
+++ project1/script.js
@@ -0,0 +1,17 @@
+const bodyEl = document.querySelector("body");
+bodyEl.addEventListener('mousemove', (event) => {
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const span = document.createElement("span");
+    span.style.left = xPos + "px";
+    span.style.top = yPos + "px";
+    const size = Math.random()*100;
+
+    span.style.width = size + "px";
+    span.style.height = size + "px";
+    bodyEl.appendChild(span);
+    setTimeout(() => {
+        span.remove();
+
+    }, 3000)
+})
